32c3676b39aff0659e83edb6fc997f7015c0e07b
maxinepqk <maxinepqk@gmail.com>
2018-02-10 15:07:05 -0500

    Keyboard input in different language sequence added

--- keyboardController.py
+++ keyboardController.py
@@ -1,7 +1,7 @@
 from pynput.keyboard import Key, Controller
 from pynput import keyboard
 import string
-from glosbe_mod import converter
+from glosbe_mod import check_dictionary
 from engkor_combined import *
 
 my_keyboard = Controller()
@@ -15,12 +15,24 @@ def switch_input_source():
     my_keyboard.release(Key.ctrl)
     my_keyboard.release(Key.alt)
 
+def delete_word(actual_word):
+    for i in range(0, len(actual_word)+1):
+        my_keyboard.press(Key.backspace)
+        my_keyboard.release(Key.backspace)
+
 def check_word(actual_word):
     (eng_valid, _) = check_dictionary(actual_word)
-    print(eng_valid, kor_valid)
-    if (!(eng_valid)) korWord = engTypeToKor(actual_word)
-    (_, kor_valid) = check_dictionary(korWord)
-    if (kor_valid) switch_input_source
+    #print(eng_valid, kor_valid)
+    if (eng_valid == False):
+        #korWord = engTypeToKor(actual_word)
+        korWord = "여보"
+        (_, kor_valid) = check_dictionary(korWord)
+        if (kor_valid):
+            print(actual_word)
+            delete_word(actual_word) #deletes english word
+            switch_input_source() #switches to korean keyboard
+            my_keyboard.type(actual_word) #types out sequence
+            switch_input_source() #switches back
 
 def on_press(key):
     try:
@@ -33,11 +45,10 @@ def on_release(key):
     print('{0} released'.format(key))
     if (key == keyboard.Key.space):
         actual_word = ''.join(word)
-        print(actual_word)
+        #print(actual_word)
         check_word(actual_word)
-
+        print("cleared")
         word.clear()
-        switch_input_source()
 
     if key == keyboard.Key.esc:
         # Stop listener
